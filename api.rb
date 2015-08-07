b63743e8873e5a2fe013ca643e6591545b249e61
Thibauld Favre <tfavre@gmail.com>
2015-08-06 23:17:29 -0400

    * 1st version of wufoo/contributor

--- api.rb
+++ api.rb
@@ -17,17 +17,16 @@ WUFHS = ENV['WUFHS']
 SLCKHOST = ENV['SLCKHOST']
 SLCKPATH = ENV['SLCKPATH']
 
-$db=Mongo::Client.new(DBURL)
-$supporteurs=$db[:supporteurs]
-$communes=$db[:communes]
-
 module Democratech
 	class API < Grape::API
 		prefix 'api'
 		version 'v1'
+		class << self
+			attr_accessor :db
+		end
 
 		helpers do
-			def slack_notification(msg,channel="#supporteurs",from="democratech",icon=":ghost:")
+			def slack_notification(msg,channel="#supporteurs",icon=":ghost:",from="democratech")
 				uri = URI.parse(SLCKHOST)
 				http = Net::HTTP.new(uri.host, uri.port)
 				http.use_ssl = true
@@ -58,6 +57,7 @@ module Democratech
 
 			get 'subscriber' do
 				# only required for mailchimp url validator
+				API.db[:communes].find({:postalCode=>'67210'}).first
 			end
 
 			post 'subscriber' do
@@ -66,7 +66,7 @@ module Democratech
 				if params["data"]["merges"]["CITY"].empty? and not params["data"]["merges"]["ZIPCODE"].empty? then
 					zip=params["data"]["merges"]["ZIPCODE"].strip.gsub(/\s+/,"")
 					if not zip.match('^[0-9]{5}(?:-[0-9]{4})?$').nil? then
-						commune=$communes.find({"postalCode"=>zip}).first
+						commune=API.db[:communes].find({:postalCode=>zip}).first
 						if not commune.nil? then
 							uri = URI.parse(MCURL)
 							http = Net::HTTP.new(uri.host, uri.port)
@@ -89,7 +89,7 @@ module Democratech
 					params['HandshakeKey']==WUFHS
 				end
 			end
-			post 'entry' do
+			post 'supporter' do
 				error!('401 Unauthorized', 401) unless authorized
 				doc={
 					:firstName=>params["Field9"].capitalize,
@@ -102,12 +102,77 @@ module Democratech
 					:created=> Time.now.utc
 				}
 				if doc[:city].empty? and not doc[:postalCode].empty? then
-					commune=$communes.find({"postalCode"=>doc[:postalCode]}).first
+					commune=API.db[:communes].find({:postalCode=>doc[:postalCode]}).first
 					doc[:city]=commune['name'] unless commune.nil?
 				end
-				$db[:supporteurs].insert_one(doc)
-				slack_notification("Nouveau supporteur ! %s %s (%s, %s, %s) : %s","channel"=>"#supporteurs","icon"=>":thumbsup")
+				begin
+					res=API.db[:supporteurs].insert_one(doc)
+					raise Exception.new(res.inspect) unless res.n==1
+					slack_notification(
+						"Nouveau supporteur ! %s %s (%s, %s, %s) : %s" % [doc[:firstName],doc[:lastName],doc[:postalCode],doc[:city],doc[:country],doc[:reason]],
+						"#supporteurs",
+						":thumbsup:"
+					)
+				rescue Exception=>e
+					slack_notification(
+						"Erreur lors de l'enregistrement d'un nouveau supporteur: %s ! %s %s (%s, %s, %s) : %s\nError msg: %s\nError trace: %s" % [doc[:email],doc[:firstName],doc[:lastName],doc[:postalCode],doc[:city],doc[:country],doc[:reason],e.message,e.backtrace.inspect[0..200]],
+						"@thibauld",
+						":scream:"
+					)
+				end
+			end
+			post 'contributor' do
+				error!('401 Unauthorized', 401) unless authorized
+				email=params["Field2"].downcase
+				note=params["Field211"]
+				tags=[]
+				tags.push("ambassadeur") unless params["Field213"].empty?
+				tags.push("event") unless params["Field214"].empty?
+				tags.push("visibility") unless params["Field215"].empty?
+				tags.push("presse") unless (params["Field314"].empty? and params["Field315"].empty?)
+				tags.push("creatif graphique") unless params["Field415"].empty?
+				tags.push("creatif video") unless params["Field416"].empty?
+				tags.push("content") unless params["Field417"].empty?
+				tags.push("seo") unless params["Field418"].empty?
+				tags.push("donateur") unless params["Field516"].empty?
+				tags.push("fundraiser") unless params["Field517"].empty?
+				tags.push("fundraiser") unless params["Field517"].empty?
+				tags.push("elu") unless (params["Field617"].empty? and params["Field618"].empty?)
+				tags.push("candidat") unless (params["Field10"].empty? and params["Field11"].empty?)
+				tags.push("beta-testeur") unless params["Field110"].empty?
+				tags.push("developer") unless (params["Field112"].empty? and params["Field113"].empty?)
+				tags.push("android") unless params["Field114"].empty?
+				tags.push("ios") unless params["Field115"].empty?
+				update={
+					:contributeur=>1,
+					:dispo=>params["Field3"],
+					:tags=>tags,
+					:lastUpdated=>Time.now.utc
+				}
+				begin
+					res=API.db[:supporteurs].find({:email=>email}).update_one({'$set'=>update})
+					if res.n==0 then
+						update[:email]=email
+						update[:created]=Time.now.utc
+						res=API.db[:supporteurs].insert_one(update)
+						raise Exception.new(res.inspect) unless res.n==1
+					end
+					slack_notification(
+						"Nouveau contributeur !\nDispo: %s\nTags: %s\nNote: %s" % [update[:dispo],tags.inspect,note],
+						"#supporteurs",
+						":muscle:"
+					)
+				rescue Exception=>e
+					slack_notification(
+						"Erreur lors de l'enregistrement d'un nouveau contributeur !\nEmail: %s\nDispo: %s\nTags: %s\nError msg: %s\nError trace: %s" % [email,update[:dispo],tags.inspect,e.message,e.backtrace.inspect[0..200]+"..."],
+						"@thibauld",
+						":fearful:"
+					)
+				end
 			end
 		end
 	end
 end
+
+Democratech::API.db=Mongo::Client.new(DBURL)
+
