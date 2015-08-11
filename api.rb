d8dc24075b96aefdc9eea89d94f90b633f19b81b
Thibauld Favre <tfavre@gmail.com>
2015-08-11 01:35:27 -0400

    v1.0.0 de l'API

--- api.rb
+++ api.rb
@@ -27,40 +27,19 @@ module Democratech
 					"text"=> msg,
 					"icon_emoji"=>icon
 				})
-				http.request(request)
-			end
-		end
-
-		resource :mailchimp do
-			helpers do
-				def authorized
-					params['HandshakeKey']==WUFHS
+				res=http.request(request)
+				if not res.kind_of? Net::HTTPSuccess then
+					puts "An error occurred trying to send a Slack notification\n"
 				end
 			end
 
-			get 'subscriber' do
-				# only required for mailchimp url validator
-			end
-
-			post 'subscriber' do
-				# update the city of a subscriber when a subscriber is added
-				# DOES NOT WORK because we lack the subscriber ID in the params :( 
-				if params["data"]["merges"]["CITY"].empty? and not params["data"]["merges"]["ZIPCODE"].empty? then
-					zip=params["data"]["merges"]["ZIPCODE"].strip.gsub(/\s+/,"")
-					if not zip.match('^[0-9]{5}(?:-[0-9]{4})?$').nil? then
-						commune=API.db[:communes].find({:postalCode=>zip}).first
-						if not commune.nil? then
-							uri = URI.parse(MCURL)
-							http = Net::HTTP.new(uri.host, uri.port)
-							http.use_ssl = true
-							http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-							request = Net::HTTP::Patch.new("/3.0/lists/"+MCLIST+"/members/"+params["data"]["id"])
-							request.basic_auth 'hello',MCKEY
-							request.add_field('Content-Type', 'application/json')
-							request.body = JSON.dump({'merge_fields'=>{'CITY'=>commune}})
-							http.request(request)
-						end
-					end
+			def slack_notifications(notifs)
+				notifs.each do |n|
+					msg=n[0] || ""
+					chann=n[1] || "#supporteurs"
+					icon=n[2] || ":ghost:"
+					from=n[3] || "democratech"
+					slack_notification(msg,chann,icon,from)
 				end
 			end
 		end
@@ -70,41 +49,101 @@ module Democratech
 				def authorized
 					params['HandshakeKey']==WUFHS
 				end
+
+				def add_to_mailing_list(doc)
+					merge_fields={}
+					merge_fields['FNAME']=doc[:firstName] unless doc[:firstName].nil?
+					merge_fields['LNAME']=doc[:lastName] unless doc[:lastName].nil?
+					merge_fields['CITY']=doc[:city] unless doc[:city].nil?
+					merge_fields['ZIPCODE']=doc[:postalCode] unless doc[:postalCode].nil?
+					merge_fields['COUNTRY']=doc[:country] unless doc[:country].nil?
+					uri = URI.parse(MCURL)
+					http = Net::HTTP.new(uri.host, uri.port)
+					http.use_ssl = true
+					http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+					request = Net::HTTP::Post.new("/3.0/lists/"+MCLIST+"/members/")
+					request.basic_auth 'hello',MCKEY
+					request.add_field('Content-Type', 'application/json')
+					request.body = JSON.dump({
+						'email_address'=>doc[:email],
+						'status'=>'subscribed',
+						'merge_fields'=>merge_fields
+					})
+					res=http.request(request)
+					return res.kind_of? Net::HTTPSuccess,res
+				end
 			end
+
 			post 'supporter' do
 				error!('401 Unauthorized', 401) unless authorized
-				doc={
-					:firstName=>params["Field9"].capitalize,
-					:lastName=>params["Field10"].upcase,
-					:email=>params["Field1"].downcase,
-					:country=>params["Field127"].upcase,
-					:postalCode=>params["Field118"].strip.gsub(/\s+/,""),
-					:city=>params["Field130"],
-					:reason=>params["Field119"],
-					:created=> Time.now.utc
-				}
-				if doc[:city].empty? and not doc[:postalCode].empty? then
+				errors=[]
+				notifs=[]
+
+				# 1. We read the new supporter info from the parameters
+				doc={}
+				doc[:firstName]=params["Field9"].capitalize unless params["Field9"].nil?
+				doc[:lastName]=params["Field10"].upcase unless params["Field10"].nil?
+				doc[:email]=params["Field1"].downcase unless params["Field1"].nil?
+				doc[:country]=params["Field127"].upcase unless params["Field127"].nil?
+				doc[:postalCode]=params["Field118"].strip.gsub(/\s+/,"") unless params["Field118"].nil?
+				doc[:city]=params["Field130"].upcase unless params["Field130"].nil?
+				doc[:reason]=params["Field119"] unless params["Field119"].nil?
+				doc[:created]=Time.now.utc
+				if doc[:city].nil? and not doc[:postalCode].nil? then
 					commune=API.db[:communes].find({:postalCode=>doc[:postalCode]}).first
 					doc[:city]=commune['name'] unless commune.nil?
 				end
-				begin
-					res=API.db[:supporteurs].insert_one(doc)
-					raise Exception.new(res.inspect) unless res.n==1
-					slack_notification(
+
+				# 2. we subscribe the new supporter to our mailchimp mailing list
+				success,res=add_to_mailing_list(doc)
+				if success then
+					# we retrieve the subscriber ID from the newly created mailchimp entry
+					mailchimp_id=JSON.parse(res.body)["id"]
+					notifs.push([
+						"Enregistrement du nouveau supporteur (%s %s) sur mailchimp OK !" % [doc[:firstName],doc[:lastName]],
+						"#supporteurs",
+						":monkey_face:"
+					])
+				else
+					notifs.push([
+						"Erreur lors de l'enregistrement d'un nouveau supporteur sur mailchimp ! [CODE: %s]" % [res.code],
+						"#errors",
+						":speak_no_evil:"
+					])
+					errors.push('400 Supporter could not be subscribed')
+				end
+
+				# 3. We register the new supporter into the DB (with his mailchimp id if subscription was successful)
+				doc[:mailchimp_id]=mailchimp_id unless mailchimp_id.nil?
+				insert_res=API.db[:supporteurs].insert_one(doc)
+				if insert_res.n==1 then
+					notifs.push([
 						"Nouveau supporteur ! %s %s (%s, %s, %s) : %s" % [doc[:firstName],doc[:lastName],doc[:postalCode],doc[:city],doc[:country],doc[:reason]],
 						"#supporteurs",
 						":thumbsup:"
-					)
-				rescue Exception=>e
-					slack_notification(
-						"Erreur lors de l'enregistrement d'un nouveau supporteur: %s ! %s %s (%s, %s, %s) : %s\nError msg: %s\nError trace: %s" % [doc[:email],doc[:firstName],doc[:lastName],doc[:postalCode],doc[:city],doc[:country],doc[:reason],e.message,e.backtrace.inspect[0..200]],
-						"@thibauld",
+					])
+				else # if the supporter could not be insert in the db
+					notifs.push([
+						"Erreur lors de l'enregistrement d'un nouveau supporteur: %s ! %s %s (%s, %s, %s) : %s\nError msg: %s\nError trace: %s" % [doc[:email],doc[:firstName],doc[:lastName],doc[:postalCode],doc[:city],doc[:country],doc[:reason],insert_res.inspect],
+						"#errors",
 						":scream:"
-					)
+					])
+					errors.push('400 Supporter could not be registered')
+				end
+
+				# 4. We send the notifications and return
+				slack_notifications(notifs)
+				if not errors.empty? then
+					error!(errors.join("\n"),400)
 				end
 			end
+
 			post 'contributor' do
 				error!('401 Unauthorized', 401) unless authorized
+				errors=[]
+				notifs=[]
+
+				# 1. We read the contributor info from the parameters
 				email=params["Field2"].downcase
 				note=params["Field211"]
 				tags=[]
@@ -118,38 +157,122 @@ module Democratech
 				tags.push("seo") unless params["Field418"].empty?
 				tags.push("donateur") unless params["Field516"].empty?
 				tags.push("fundraiser") unless params["Field517"].empty?
-				tags.push("fundraiser") unless params["Field517"].empty?
 				tags.push("elu") unless (params["Field617"].empty? and params["Field618"].empty?)
 				tags.push("candidat") unless (params["Field10"].empty? and params["Field11"].empty?)
 				tags.push("beta-testeur") unless params["Field110"].empty?
+				tags.push("designer") unless params["Field111"].empty?
 				tags.push("developer") unless (params["Field112"].empty? and params["Field113"].empty?)
 				tags.push("android") unless params["Field114"].empty?
 				tags.push("ios") unless params["Field115"].empty?
+
+				# 2. we update the subscriber record with the contributor's tags
 				update={
 					:contributeur=>1,
 					:dispo=>params["Field3"],
 					:tags=>tags,
 					:lastUpdated=>Time.now.utc
 				}
-				begin
-					res=API.db[:supporteurs].find({:email=>email}).update_one({'$set'=>update})
-					if res.n==0 then
-						update[:email]=email
-						update[:created]=Time.now.utc
-						res=API.db[:supporteurs].insert_one(update)
-						raise Exception.new(res.inspect) unless res.n==1
+				supporter=API.db[:supporteurs].find({:email=>email}).find_one_and_update({'$set'=>update}) # returns the document found
+				mailchimp_id=supporter['mailchimp_id']
+
+				# 3. if no supporter was found then we register him (can be the case if the contributor did not sign the initial form)
+				if supporter.nil? then
+					update[:email]=email
+					update[:created]=Time.now.utc
+					insert_res=API.db[:supporteurs].insert_one(update)
+					if insert_res.n==1 then
+						notifs.push([
+							"Nouveau supporteur ET contributeur !\nDispo: %s\nTags: %s\nNote: %s" % [update[:dispo],tags.inspect,note],
+							"#supporteurs",
+							":muscle:"
+						])
+					else
+						notifs.push([
+							"Erreur lors de l'enregistrement d'un nouveau contributeur !\nEmail: %s\nDispo: %s\nTags: %s\nError msg: %s" % [email,update[:dispo],tags.inspect,insert_res.inspect],
+							"#errors",
+							":fearful:"
+						])
+						errors.push('400 Contributor not registered and cannot be registered')
 					end
-					slack_notification(
+					
+					# 4. If no supporter was found then we subscribe him on our mailchimp mailing list
+					success,res=add_to_mailing_list(update)
+					if success then
+						# we retrieve the subscriber ID from the newly created mailchimp entry
+						mailchimp_id=JSON.parse(res.body)["id"]
+						notifs.push([
+							"Enregistrement d'un nouveau supporteur sur mailchimp !",
+							"#supporteurs",
+							":monkey_face:"
+						])
+					else
+						notifs.push([
+							"Erreur lors de l'enregistrement d'un nouveau supporteur sur mailchimp ! [CODE: %s]" % [res.code],
+							"#errors",
+							":speak_no_evil:"
+						])
+						errors.push('400 New supporter could not be subscribed')
+					end
+
+				else
+					notifs.push([
 						"Nouveau contributeur !\nDispo: %s\nTags: %s\nNote: %s" % [update[:dispo],tags.inspect,note],
 						"#supporteurs",
 						":muscle:"
-					)
-				rescue Exception=>e
-					slack_notification(
-						"Erreur lors de l'enregistrement d'un nouveau contributeur !\nEmail: %s\nDispo: %s\nTags: %s\nError msg: %s\nError trace: %s" % [email,update[:dispo],tags.inspect,e.message,e.backtrace.inspect[0..200]+"..."],
-						"@thibauld",
-						":fearful:"
-					)
+					])
+				end
+
+				# 5. We retrieve the groups of the mailchimp mailing list and match them to the tags of the contributor
+				if not mailchimp_id.nil? and not mailchimp_id.empty? then
+					uri = URI.parse(MCURL)
+					http = Net::HTTP.new(uri.host, uri.port)
+					http.use_ssl = true
+					http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+					request = Net::HTTP::Get.new("/3.0/lists/"+MCLIST+"/interest-categories/"+MCGROUPCAT+"/interests")
+					request.basic_auth 'hello',MCKEY
+					res=http.request(request)
+					response=JSON.parse(res.body)["interests"]
+					groups={}
+					response.each do |i|
+						if (tags.include? i["name"].downcase) then
+							groups[i["id"]]=true
+						else
+							groups[i["id"]]=false
+						end
+					end
+
+					# 6. We update the subscriber on mailchimp to reflect the tags of the contributor
+					uri = URI.parse(MCURL)
+					http = Net::HTTP.new(uri.host, uri.port)
+					http.use_ssl = true
+					http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+					request = Net::HTTP::Patch.new("/3.0/lists/"+MCLIST+"/members/"+mailchimp_id)
+					request.basic_auth 'hello',MCKEY
+					request.add_field('Content-Type', 'application/json')
+					request.body = JSON.dump({
+						'interests'=>groups
+					})
+					res=http.request(request)
+					if res.kind_of? Net::HTTPSuccess then
+						notifs.push([
+							"Supporter mis a jour dans mailchimp. Tags: %s" % [tags.inspect],
+							"#supporteurs",
+							":monkey_face:"
+						])
+					else
+						notifs.push([
+							"Erreur lors de la mise a jour du supporter dans mailchimp. Tags: %s" % [tags.inspect],
+							"#errors",
+							":speak_no_evil:"
+						])
+						errors.push('400 Supporter could not be updated in mailchimp')
+					end
+				end
+
+				# 4. We send the notifications and return
+				slack_notifications(notifs)
+				if not errors.empty? then
+					error!(errors.join("\n"),400)
 				end
 			end
 		end
--- config.ru
+++ config.ru
@@ -1,14 +1,15 @@
+require './keys.local.rb'
 require './api.rb'
 require 'unicorn'
 
-DBURL = ENV['DBURL']
-MCKEY = ENV['MCKEY']
-MCURL = ENV['MCURL']
-MCLIST = ENV['MCLIST']
-MCFHS = ENV['MCFHS']
-WUFHS = ENV['WUFHS']
-SLCKHOST = ENV['SLCKHOST']
-SLCKPATH = ENV['SLCKPATH']
+#DBURL = ENV['DBURL']
+#MCKEY = ENV['MCKEY']
+#MCURL = ENV['MCURL']
+#MCLIST = ENV['MCLIST']
+#MCFHS = ENV['MCFHS']
+#WUFHS = ENV['WUFHS']
+#SLCKHOST = ENV['SLCKHOST']
+#SLCKPATH = ENV['SLCKPATH']
 
 Mongo::Logger.logger.level = Logger::WARN
 Democratech::API.db=Mongo::Client.new(DBURL)
