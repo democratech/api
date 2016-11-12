9f83998970e0e8e2b357e7018fd1eea4557be286
Thibauld Favre <tfavre@gmail.com>
2016-11-12 12:13:27 -0500

    envoi nouvel email pour 2nd tour

--- api/citizen_v1.rb
+++ api/citizen_v1.rb
@@ -150,7 +150,7 @@ END
 						])
 					else
 						citoyen=res[0]
-						info="Lien de vote envoyé à %s %s" % [citoyen['firstname'],citoyen['lastname']]
+						info="Lien de vote 2nd tour envoyé à %s %s" % [citoyen['firstname'],citoyen['lastname']]
 						API.log.info(info)
 						notifs.push([
 							info,
@@ -196,7 +196,7 @@ END
 							}]
 						}]
 					}
-					result=API.mandrill.messages.send_template("laprimaire-org-1er-tour-de-vote",[],message)
+					result=API.mandrill.messages.send_template("laprimaire-org-pre-2nd-tour-de-vote",[],message)
 				rescue Mandrill::Error => e
 					msg="A mandrill error occurred: #{e.class} - #{e.message}"
 					API.log.error(msg)
